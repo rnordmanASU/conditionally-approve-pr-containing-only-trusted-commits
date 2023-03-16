de11675e58e7db0538b80940884cf8a24258464c
Robert Nordman <rnordman@asu.edu>
2023-03-16 08:24:36 -0700

    beginning of how to check using nodejs

--- index.js
+++ index.js
@@ -1,19 +1,85 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
+const github = require('@actions/github');
+const core = require('@actions/core');
+
+async function all_commits_trusted(client, pr) {
+    const trustedBraches = core.getInput('trusted-branches', { required: true });
+
+    // Get list of commits on a PR
+    const changeCommits = await client.pulls.listCommits({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        pull_number: pr.number,
+    }).data;
+
+    const trustedCommitsByBranch = await client.repos.listCommits({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        branch: pr.number,
+    }).data;
+
+    for (let trustedBrach of trustedBraches) {
+        const trustedCommits = await client.repos.listCommits({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            branch: trustedBrach,
+        }).data;
+
+        trustedCommitHashes = []
+        for (let commit of listCommits) {
+            trustedCommitHashes.push(commit.sha);
+        }
+    }
+
+
+    // Get all committers on a PR
+    for (let commit of listCommits) {
+        // Check if there are committers other than ALLOWED_COMMITTERS
+        if (!fromTrustedBranch(trustedBraches, commit.sha)) {
+            core.info(
+                `Commit ${commit.sha} is not from a trusted branch)`
+            );
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+async function fromTrustedBranch(trustedBraches, sha) {
+
+}
+
+github.context = {
+    'payload': {
+        'pull_request': {
+            'number' : 31
+        }
+    },
+    'repo': {
+        'repo': 'auto-approve-into-main-if-approved-in-qa',
+        'owner': 'rnordmanASU'
+    }
+};
+const orgGetInput = core.getInput;
+core.getInput = (key, opt) => {
+    if (key === 'github-token') return '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';
+    else if (key === 'trusted-branches') return 'mule-qa';
+    else return orgGetInput(key, opt)
+}
 
 async function run() {
     try {
         const token = core.getInput('github-token', { required: true });
-        const trustedBraches = core.getInput('trusted-branches', { required: true });
 
         const { pull_request: pr } = github.context.payload;
         if (!pr) {
             throw new Error('Event payload missing `pull_request`');
         }
 
-        const client = new github.GitHub(token);
+        const octoclient = new github.getOctokit(token);
+        const client = octoclient.rest;
 
-        if (!(await all_committers_allowed(client, pr))) return;
+        if (!(await all_commits_trusted(client, pr))) return;
 
         core.debug(`Creating approving review for pull request #${pr.number}`);
         await client.pulls.createReview({
